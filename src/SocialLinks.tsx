2bbd938dea4fa17f3fc4be0920dc39151258d14f
katelynn-n6 <katelynnnguyen6@gmail.com>
2024-09-27 18:44:34 -0400

    Update SocialLinks.tsx

--- src/SocialLinks.tsx
+++ src/SocialLinks.tsx
@@ -1,10 +1,10 @@
 export default function SocialLinks() {
   return (
     <div className="relative flex text-black">
-      <a rel="noopener noreferrer" href="https://linkedin.com/in/katelynn-n6">
+      <a target="_blank" href="https://linkedin.com/in/katelynn-n6">
         <img className="size-5" src="/linkedin.png"></img>
       </a>
-      <a rel="noopener noreferrer" href="https://github.com/katelynn-n6">
+      <a target="_blank" href="https://github.com/katelynn-n6">
         <img className="size-5 ml-4" src="/github.png"></img>
       </a>
     </div>
